43e578fbcedd302a328ae2e53cc729abd04e84d3
Saraswathi <96658906+SaraswathiVaindam@users.noreply.github.com>
2023-06-27 23:52:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+// typing text animation script
+var typed = new Typed(".typing", {
+    strings: ["", "Web Developer", "Web Designer", "Student", "Programmer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-1", {
+    strings: ["", "Web Developer", "Web Designer", "Student", "Programmer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+
+
+//  ARROW SCROLL TO UP
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset+height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    //    STICKY NAVBAR
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+};
+
+
+//       SCROLL REVEAL
+
+ScrollReveal({
+    reset: true,
+    distance: '50px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.header, .my-skill-text, .skill-title, .project-title', {origin: 'top'});
+ScrollReveal().reveal('.main-content, .my-skill-card, .my-skills, .skills, .projects, .contact, form', {origin: 'bottom'});
+ScrollReveal().reveal('.profile-img, .left', {origin: 'left'});
+ScrollReveal().reveal('.media-body, .right', {origin: 'right'});
